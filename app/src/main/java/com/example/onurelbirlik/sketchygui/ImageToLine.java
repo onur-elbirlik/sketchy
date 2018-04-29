fdef12cdfce8e4a2d34e9413360ae9d74b2b2558
atadnmz <kadiratadonmez@gmail.com>
2018-04-29 17:03:44 +0300

    Save button was added to ImageToLine class and rendered image can be saved to external storage.

--- app/src/main/java/com/example/onurelbirlik/sketchygui/ImageToLine.java
+++ app/src/main/java/com/example/onurelbirlik/sketchygui/ImageToLine.java
@@ -1,15 +1,20 @@
 package com.example.onurelbirlik.sketchygui;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import org.opencv.android.OpenCVLoader;
@@ -20,7 +25,13 @@ import org.opencv.core.Mat;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
 public class ImageToLine extends AppCompatActivity {
+    Bitmap bm;
     static{
         if(!OpenCVLoader.initDebug()){
             Log.i("opencv","init basarisiz");
@@ -54,7 +65,7 @@ public class ImageToLine extends AppCompatActivity {
     }
     public void detectEdges(int th) {
         ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.doge);
+        bm = BitmapFactory.decodeResource(getResources(), R.drawable.doge);
         Mat img = new Mat();
         Utils.bitmapToMat(bm,img);
         Mat dest = new Mat();
@@ -66,6 +77,33 @@ public class ImageToLine extends AppCompatActivity {
         //BURDA KALDIN
         Utils.matToBitmap(dest2,bm);
         imageView.setImageBitmap(bm);
+    }
+    private void saveImage(Bitmap finalBitmap) {
+        String folderPath = Environment.getExternalStorageDirectory() + "/SketchyPhoto";
+        System.out.println(folderPath);
+        File direct = new File(folderPath);
+
+        if (!direct.exists()) {
+            File wallpaperDirectory = new File(folderPath);
+            wallpaperDirectory.mkdirs();
+        }
+
+        File file = new File(folderPath, "trial1");
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bm.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void saveButton(View view){
+        System.out.println("save success!!");
+        saveImage(bm);
     }
 
 }
--- openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\ATA\\Desktop\\opencv\\sketchy-SketchyGUI\\sketchy-SketchyGUI\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\ATA\\Desktop\\SketchyFinal\\sketchy\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
