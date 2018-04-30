2ec7d3729b52df4f7f6819e6a615b26fd23ec1d0
alppeh <alppehlivanoglu@gmail.com>
2018-04-30 22:56:33 +0300

    Update HomeActivity.java

--- app/src/main/java/com/example/onurelbirlik/sketchygui/HomeActivity.java
+++ app/src/main/java/com/example/onurelbirlik/sketchygui/HomeActivity.java
@@ -1,17 +1,62 @@
 package com.example.onurelbirlik.sketchygui;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.os.SystemClock;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button galleryButton;
+    ImageView imageView;
+    private static final int PICK_IMAGE = 100;
+    Uri imageURL;
+    public static Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        galleryButton = findViewById(R.id.selectButton);
+        imageView = findViewById(R.id.imageView);
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openGallery();
+            }
+        });
+    }
+    private void openGallery(){
+
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
+            imageURL = data.getData();
+            imageView.setImageURI(imageURL);
+            imageView.setVisibility(View.INVISIBLE);
+            BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+            bitmap = drawable.getBitmap();
+            Intent tempIntent = new Intent(HomeActivity.this, ImageToLine.class);
+            startActivity(tempIntent);
+        }
     }
     private long mLastClickTime = 0;
     public void goTo(View view)
@@ -20,12 +65,12 @@ public class HomeActivity extends AppCompatActivity {
             return;
         }
         mLastClickTime = SystemClock.elapsedRealtime();
-        Intent intent = new Intent(HomeActivity.this, ImageToLine.class);
+        Intent intent = new Intent(HomeActivity.this, HomeActivity.class);
         startActivity(intent);
     }
     public void goToDisplayActivity(View view)
     {
-        Intent intent = new Intent(HomeActivity.this, DisplayActivity.class);
+        Intent intent = new Intent(HomeActivity.this, ImageToLine.class);
         startActivity(intent);
     }
 }
