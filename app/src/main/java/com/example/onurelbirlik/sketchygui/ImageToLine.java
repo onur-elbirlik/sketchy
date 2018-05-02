fc347a132fff03684ce3c09650fa6d2c0c4825ea
atadnmz <kadiratadonmez@gmail.com>
2018-05-03 02:16:45 +0300

    Merge branch 'master' of C:\Users\ATA\Desktop\SketchyFinal\sketchy with conflicts.

--- app/src/main/java/com/example/onurelbirlik/sketchygui/ImageToLine.java
+++ app/src/main/java/com/example/onurelbirlik/sketchygui/ImageToLine.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
@@ -25,31 +26,36 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.photo.Photo;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
 public class ImageToLine extends AppCompatActivity {
     Bitmap bm = HomeActivity.bitmap;
+    Bitmap bmCopy;
     static{
         if(!OpenCVLoader.initDebug()){
-            Log.i("opencv","init basarisiz");
+            Log.i("opencv","init fail");
         }else{
-            Log.i("opencv","init basarili");
+            Log.i("opencv","init success");
         }
     }
-
     private int threshHold =300;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(bm.getWidth()>1200||bm.getHeight()>1200)
+            bm=Bitmap.createScaledBitmap(bm,1000, 1000, true);
         setContentView(R.layout.activity_image_to_line);
-
+        detectEdges(100);
         SeekBar s1 = (SeekBar) findViewById(R.id.seekBar3);
         s1.setProgress(100);
         threshHold=s1.getProgress();
@@ -68,69 +74,62 @@ public class ImageToLine extends AppCompatActivity {
         });
     }
     public void detectEdges(int th) {
+
         ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-        //bm = BitmapFactory.decodeResource(getResources(), R.drawable.doge);
+        bmCopy = bm.copy(bm.getConfig(),true);
         Mat input = new Mat();
-        Utils.bitmapToMat(bm,input);
+        Utils.bitmapToMat(bmCopy,input);
         Mat outputGauss = new Mat();
         Mat outputCanny = new Mat();
 
         Imgproc.GaussianBlur(input, outputGauss, new Size(7, 7), 0);
         Imgproc.Canny(outputGauss, outputCanny, 0, th);
+
         Core.bitwise_not(outputCanny,outputCanny);
-        Imgproc.erode(outputCanny, outputCanny, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(4, 4)));
-
-
-
+        Imgproc.erode(outputCanny, outputCanny, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2, 2)));
         //green mask aplied
-        //Mat mask = new Mat(dest2.size(),CvType.CV_8UC3);
-        //Imgproc.threshold(dest2,mask,0,255,Imgproc.THRESH_BINARY_INV|Imgproc.THRESH_OTSU);
-
-        Imgproc.cvtColor(outputCanny,outputCanny,Imgproc.COLOR_GRAY2BGR);
-        //dest2.setTo(new Scalar(0,0,255),mask);
-
-        Mat inputRGBA = new Mat(outputCanny.size(),CvType.CV_8UC4);
-
-        Imgproc.cvtColor(outputCanny,inputRGBA,Imgproc.COLOR_BGR2BGRA);
-        int thres = 245;
-        double [] pixels = new double[4];
-        /*for(int i=0;i<inputRGBA.rows();i++){
-            for(int k=0;k<inputRGBA.cols();k++){
-                if(inputRGBA.get(i,k)[0]>=thres&&inputRGBA.get(i,k)[1]>=thres&&inputRGBA.get(i,k)[2]>=thres){
-                    pixels[0]=inputRGBA.get(i,k)[0];
-                    pixels[1]=inputRGBA.get(i,k)[1];
-                    pixels[2]=inputRGBA.get(i,k)[2];
-                    pixels[3]=0;
-
-                    inputRGBA.put(i,k,pixels);
-
-
-                }
-                //System.out.println(inputRGBA.get(i,k)[0]+" "+inputRGBA.get(i,k)[1]+" "+inputRGBA.get(i,k)[2]+" "+inputRGBA.get(i,k)[3]);
-
-            }
-        }
-        //System.out.println(inputRGBA.get(100,100)[0]+" "+inputRGBA.get(100,100)[1]+" "+inputRGBA.get(100,100)[3]);*/
-
-        Utils.matToBitmap(outputCanny,bm);
-        imageView.setImageBitmap(bm);
+        //Mat mask = new Mat(outputCanny.size(),CvType.CV_8UC3);
+        //Imgproc.threshold(outputCanny,mask,0,255,Imgproc.THRESH_BINARY_INV|Imgproc.THRESH_OTSU);
+        //Imgproc.cvtColor(outputCanny,outputCanny,Imgproc.COLOR_GRAY2BGR);
+        //outputCanny.setTo(new Scalar(0,0,255),mask);
+        Utils.matToBitmap(outputCanny,bmCopy);
+        //imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setImageBitmap(bmCopy);
     }
+    public static Bitmap createTransparentBitmapFromBitmap(Bitmap bitmap,
+                                                           int replaceThisColor) {
+        if (bitmap != null) {
+            int picw = bitmap.getWidth();
+            int pich = bitmap.getHeight();
+            int[] pix = new int[picw * pich];
+            bitmap.getPixels(pix, 0, picw, 0, 0, picw, pich);
 
+            for (int y = 0; y < pich; y++) {
+                // from left to right
+                for (int x = 0; x < picw; x++) {
+                    int index = y * picw + x;
 
-
-
-
-    private void saveImage(Bitmap finalBitmap) {
-        String folderPath = Environment.getExternalStorageDirectory() + "/SketchyPhoto";
+                    if (pix[index] == replaceThisColor) {
+                        pix[index] = Color.TRANSPARENT;
+                    }
+                }
+            }
+            Bitmap bm = Bitmap.createBitmap(pix, picw, pich,
+                    Bitmap.Config.ARGB_4444);
+            return bm;
+        }
+        return null;
+    }
+    private void saveImage(Bitmap finalBitmap){
+        String folderPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+"/SketchyPhoto";
         System.out.println(folderPath);
         File direct = new File(folderPath);
-
         if (!direct.exists()) {
             File wallpaperDirectory = new File(folderPath);
             wallpaperDirectory.mkdirs();
         }
-
-        File file = new File(folderPath, "trial2.png");
+        File file = new File(folderPath, "SketchtPhoto"+ HomeActivity.saveCounter +".png");
+        HomeActivity.saveCounter++;
         if (file.exists()) {
             file.delete();
         }
@@ -152,26 +151,19 @@ public class ImageToLine extends AppCompatActivity {
     }
     public void saveButton(View view){
         if (isExternalStorageWritable()) {
-            System.out.println("save success!!");
+            bm=bmCopy;
             saveImage(bm);
+            //bm=createTransparentBitmapFromBitmap(bm,Color.WHITE);
         }else{
-            System.out.println("save fail!");
+            Log.i("save","save fail");
         }
     }
     public void aRButton(View view){
-        String path = Environment.getExternalStorageDirectory()+ "/SketchyPhoto/trial2.png";
-        File imgFile = new File(path);
-        if(imgFile.exists()){
-            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-
-            //Bitmap bm3 = BitmapFactory.decodeResource(getResources(), R.drawable.gh);
-
-            BoxRenderer.setBitmap(myBitmap);
-            Intent intent = new Intent(ImageToLine.this, DisplayActivity.class);
-            startActivity(intent);
-        } else {
-            System.out.println("file does not exist!");
-        }
+        bm=bmCopy;
+        bm=createTransparentBitmapFromBitmap(bm,Color.WHITE);
+        BoxRenderer.setBitmap(bm);
+        Intent intent = new Intent(ImageToLine.this, DisplayActivity.class);
+        startActivity(intent);
     }
 
 }
