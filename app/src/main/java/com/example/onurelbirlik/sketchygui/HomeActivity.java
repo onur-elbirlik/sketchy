8febc55ee6cbf791b98ef1b792ba6d0135153725
alppeh <alppehlivanoglu@gmail.com>
2018-05-06 22:51:21 +0300

    Updated Home Activity, New navigation for start button

--- app/src/main/java/com/example/onurelbirlik/sketchygui/HomeActivity.java
+++ app/src/main/java/com/example/onurelbirlik/sketchygui/HomeActivity.java
@@ -14,63 +14,38 @@ import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button galleryButton,credits;
-    ImageView imageView;
-    private static final int PICK_IMAGE = 100;
-    Uri imageURL;
-    public static Bitmap bitmap;
+    Button credits,start;
     static int saveCounter=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        galleryButton = findViewById(R.id.startButton);
-        credits = findViewById(R.id.creditsButton);
-        imageView = findViewById(R.id.imageView);
 
+        credits = findViewById(R.id.creditsButton);
+        start = findViewById(R.id.startButton);
         credits.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openCredits();
             }
         });
-
-        galleryButton.setOnClickListener(new View.OnClickListener() {
+        start.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-
-                openGallery();
+            public void onClick(View view){
+                openTakePictureCamera();
             }
         });
-
-
     }
 
     private void openCredits(){
-        Intent TakePictureCamera = new Intent(this, TakePictureCamera.class);
-        startActivity(TakePictureCamera);
+        Intent Credits = new Intent(this, Credits.class);
+        startActivity(Credits);
     }
-    private void openGallery(){
-
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
+    private void openTakePictureCamera(){
+        Intent takePicture = new Intent(this, TakePictureCamera.class);
+        startActivity(takePicture);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
-            imageURL = data.getData();
-            imageView.setImageURI(imageURL);
-            imageView.setVisibility(View.INVISIBLE);
-            BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-            bitmap = drawable.getBitmap();
-            Intent tempIntent = new Intent(HomeActivity.this, ImageToLine.class);
-            startActivity(tempIntent);
-        }
-    }
 
     private long mLastClickTime = 0;
     public void goTo(View view)
