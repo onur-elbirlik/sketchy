3ff6362570cd0d2eaa0c2d35ad97877aad7cd420
alppeh <alppehlivanoglu@gmail.com>
2018-05-06 22:52:11 +0300

    Updated TakePictureCamera screen

--- app/src/main/java/com/example/onurelbirlik/sketchygui/TakePictureCamera.java
+++ app/src/main/java/com/example/onurelbirlik/sketchygui/TakePictureCamera.java
@@ -13,14 +13,23 @@ import android.widget.ImageView;
 
 public class TakePictureCamera extends AppCompatActivity {
 
-    ImageView imageView;
+    ImageView imageView,imageView2;
+    Button galleryButton;
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageURL;
+    public static Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture_camera);
         Button btnCamera = (Button)findViewById(R.id.btnCamera);
-        imageView = (ImageView)findViewById(R.id.imageView);
+        galleryButton = findViewById(R.id.selectImage);
+        imageView = findViewById(R.id.imageView);
+
+        imageView2 = findViewById(R.id.imageView1);
+
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,20 +40,43 @@ public class TakePictureCamera extends AppCompatActivity {
 
 
         });
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                openGallery();
+            }
+        });
+
     }
-    @Override
+    private void openGallery(){
+
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            Uri imageURL = data.getData();
-            imageView.setImageURI(imageURL);
-            imageView.setVisibility(View.INVISIBLE);
-            BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+        if( resultCode == RESULT_OK && requestCode == PICK_IMAGE)
+        {
+                imageURL = data.getData();
+                imageView.setImageURI(imageURL);
+                imageView.setVisibility(View.INVISIBLE);
+                BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+                bitmap = drawable.getBitmap();
+                Intent tempIntent = new Intent(TakePictureCamera.this, ImageToLine.class);
+                startActivity(tempIntent);
+        }
+        else if(resultCode == RESULT_OK){
+            imageURL = data.getData();
+            imageView2.setImageURI(imageURL);
+            imageView2.setVisibility(View.INVISIBLE);
+            BitmapDrawable drawable = (BitmapDrawable) imageView2.getDrawable();
             Bitmap bitmap = drawable.getBitmap();
-            HomeActivity.bitmap=bitmap;
+            TakePictureCamera.bitmap=bitmap;
             Intent tempIntent = new Intent(TakePictureCamera.this, ImageToLine.class);
             startActivity(tempIntent);
         }
-        //imageView.setImageBitmap(bitmap);
+
     }
 }
+
